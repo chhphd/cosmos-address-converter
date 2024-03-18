9b6ef81e0c46b224e936fd75dfdd0f3efdeb8710
chhphd <chhphd@gmail.com>
2024-03-18 20:02:04 +0200

    add address validation

--- ac.py
+++ ac.py
@@ -2,34 +2,26 @@ import bech32
 import argparse
 import re
 
-# # Address validation - make sure the user inputs a correct cosmos type address
-# def address_validation(prefix: str, address: str) -> bool:
+# Address validation - make sure the user inputs a correct cosmos type address
+def address_validation(address: str) -> bool:
 
-#     # Generate address regex
-#     def generate_regex(prefix: str) -> str:
-#         allowed_chars = "qpzry9x8gf2tvdw0s3jn54khce6mua7l"
-#         return f"^{prefix}1([{allowed_chars}]+)$"
+    def validate_string(address: str) -> bool:
+        regex_pattern = r'.*1[a-zA-Z0-9]{38}$'
+        return re.match(regex_pattern, address) is not None        
 
-#     # Verify address checksum
-#     def verify_checksum(address: str) -> bool:
-#         _, decoded = bech32.bech32_decode(address)
+    # Verify address checksum
+    def verify_checksum(address: str) -> bool:
+        _, decoded = bech32.bech32_decode(address)
 
-#         if decoded is not None:
-#             return len(decoded) == 32
-#         else:
-#             return False
-
-#     def is_valid_address(regex: str, address: str) -> bool:
-#         match = re.match(regex, address)
-#         if match:
-#             return verify_checksum(address)
-#         else:
-#             return False
-
-#     regex = generate_regex(prefix)
-#     validation = is_valid_address(regex, address)
-
-#     return validation
+        if decoded is not None:
+            return len(decoded) == 32
+        else:
+            return False
+        
+    if validate_string(address) and verify_checksum(address):
+        return True    
+    else:
+        return False
 
 
 def main():
@@ -49,23 +41,29 @@ def main():
     address = args.address
     to_prefix = args.to_prefix
 
-    # Decode the address
-    try:
-        prefix, canonical = bech32.bech32_decode(address)
-    except Exception as err:
-        print(f'Error parsing "{address}" as bech32 address: {err}')  
+    if address_validation(address):
 
-    # Set new prefix (default = osmosis)
-    new_prefix = prefix.replace(prefix, to_prefix)
+        # Decode the address
+        try:
+            prefix, canonical = bech32.bech32_decode(address)
+        except Exception as err:
+            print(f'Error parsing "{address}" as bech32 address: {err}')  
 
-    # Encode the address
-    try:
-        new_address = bech32.bech32_encode(new_prefix, canonical)
-    except Exception as err:
-        print(f'Error converting "{address}" to "{to_prefix}" prefix: {err}')
+        # Set new prefix (default = osmosis)
+        new_prefix = prefix.replace(prefix, to_prefix)
 
-    print(f'Original address : {address}')
-    print(f'Converted address: {new_address}')
+        # Encode the address
+        try:
+            new_address = bech32.bech32_encode(new_prefix, canonical)
+        except Exception as err:
+            print(f'Error converting "{address}" to "{to_prefix}" prefix: {err}')
+
+        print(f'Original address : {address}')
+        print(f'Converted address: {new_address}')
+
+    else:    
+        print('Please input a valid cosmos type address!')
+        exit()
 
 # Main
 if __name__ == "__main__":
